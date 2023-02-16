3f831e51a1b56ec28549c11c5e48d3969026630c
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2023-02-15 21:29:33 -0500

    delete vertex

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Mesh.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/Mesh.java
@@ -2,11 +2,8 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-
-import ca.mcmaster.cas.se2aa4.a2.generator.Vertex;
 
 public class Mesh {
 
