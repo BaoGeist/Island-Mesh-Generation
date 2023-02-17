b6eaec3fd5d545f5869099c57d5107fa1eb321da
Baoze Lin <linofbaoze@outlook.com>
2023-02-17 13:42:37 -0500

    Delivered F01 but better, with an ADT

--- generator/src/test/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGenTest.java
+++ generator/src/test/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGenTest.java
@@ -2,14 +2,18 @@ package ca.mcmaster.cas.se2aa4.a2.generator;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DotGenTest {
 
     @Test
     public void meshIsNotNull() {
-        DotGen generator = new DotGen();
+        ArrayList<Structs.Vertex> vertices = new ArrayList<>();
+        ArrayList<Structs.Segment> segments = new ArrayList<>();
+        OurMesh generator = new OurMesh(500, 500, 20, 1.00f, 1, vertices,segments);
         Structs.Mesh aMesh = generator.generate();
         assertNotNull(aMesh);
     }
