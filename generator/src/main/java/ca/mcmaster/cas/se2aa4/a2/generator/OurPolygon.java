967aac74ef846903a907dc7d892216846b08dc32
Baoze Lin <linofbaoze@outlook.com>
2023-02-17 01:07:13 -0500

    Added reference ID's in Polygon for Segments and Centroid

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/OurPolygon.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/OurPolygon.java
@@ -10,19 +10,23 @@ import java.util.List;
 
 public class OurPolygon {
     Vertex middle_vertex;
-    ArrayList<Segment> segments = new ArrayList<>();
+    ArrayList<Segment> segments_group = new ArrayList<>();
+    ArrayList<Integer> segments_id = new ArrayList<Integer>();
+    private int thickness = 1;
+    private float alpha = 1;
 
     public Polygon create_polygon(Segment... segments) {
         for(Segment segment: segments) {
-            this.segments.add(segment);
+            segments_group.add(segment);
+            segments_id.add(extractID(segment.getPropertiesList()));
         }
         middle_vertex = create_middle_vertex();
-        return build_segment();
+        return build_polygon();
     }
 
     private Vertex create_middle_vertex() {
         int totalx = 0, totaly = 0, count = 0;
-        for(Segment segment: this.segments) {
+        for(Segment segment: segments_group) {
             totalx += extractSegmentMiddle(segment.getPropertiesList())[0];
             totaly += extractSegmentMiddle(segment.getPropertiesList())[1];
             count++;
@@ -31,8 +35,11 @@ public class OurPolygon {
         return v.makeCentroidVertex(totalx/count, totalx/count);
     }
 
-    private Polygon build_segment() {
-
+    private Polygon build_polygon() {
+        Property thicc = Property.newBuilder().setKey("thicc").setValue(Integer.toString(thickness)).build();
+        Property a = Property.newBuilder().setKey("alpha").setValue(Float.toString(alpha)).build();
+        Polygon p = Polygon.newBuilder().addAllSegmentIdxs(segments_id).setCentroidIdx(extractID(middle_vertex.getPropertiesList())).addProperties(thicc).addProperties(a).build();
+        return p;
     }
 
     private int[] extractSegmentMiddle(List<Property> properties) {
@@ -44,6 +51,17 @@ public class OurPolygon {
             }
         }
         return parse_string_to_array_int(val);
+    }
+
+    private int extractID(List<Property> properties) {
+        String val = null;
+        for(Property p: properties) {
+            if (p.getKey().equals("id")) {
+//                System.out.println(p.getValue());
+                val = p.getValue();
+            }
+        }
+        return Integer.parseInt(val);
     }
 
     private int[] parse_string_to_array_int(String parse) {
--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/OurSegment.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/OurSegment.java
@@ -14,15 +14,19 @@ public class OurSegment {
     private Vertex head_vertex;
     private Vertex tail_vertex;
     private Segment actual_segment;
+    private int thickness;
+    private float alpha;
     private int id;
 
-    public Structs.Segment create_segment(int ID1, int ID2, Vertex vertex1, Vertex vertex2, int IDself) {
+    public Structs.Segment create_segment(int ID1, int ID2, Vertex vertex1, Vertex vertex2, float alpha_entry, int thickness_entry, int IDself) {
         set_segment_colour(vertex1, vertex2);
         head_vertex = vertex1;
         tail_vertex = vertex2;
         head_coord = get_coords(vertex1);
         tail_coord = get_coords(vertex2);
         id = IDself;
+        alpha = alpha_entry;
+        thickness = thickness_entry;
         return build_segment();
     }
 
@@ -51,12 +55,15 @@ public class OurSegment {
     }
 
     private Structs.Segment build_segment() {
-        Property segment_head_coords = Structs.Property.newBuilder().setKey("head").setValue(String.format("%.2f,%.2f", head_coord[0], head_coord[1])).build();
-        Property segment_tail_coords = Structs.Property.newBuilder().setKey("tail").setValue(String.format("%.2f,%.2f", tail_coord[0], tail_coord[1])).build();
-        Property segment_middle_coords = Structs.Property.newBuilder().setKey("middle").setValue(String.format("%.2f,%.2f", middle_coord[0], middle_coord[1])).build();
+        Property segment_head_coords = Property.newBuilder().setKey("head").setValue(String.format("%.2f,%.2f", head_coord[0], head_coord[1])).build();
+        Property segment_tail_coords = Property.newBuilder().setKey("tail").setValue(String.format("%.2f,%.2f", tail_coord[0], tail_coord[1])).build();
+        Property segment_middle_coords = Property.newBuilder().setKey("middle").setValue(String.format("%.2f,%.2f", middle_coord[0], middle_coord[1])).build();
+        Property segment_id = Property.newBuilder().setKey("id").setValue(Integer.toString(id)).build();
+        Property thicc = Property.newBuilder().setKey("thicc").setValue(Integer.toString(thickness)).build();
+        Property a = Property.newBuilder().setKey("alpha").setValue(Float.toString(alpha)).build();
         String new_colour1 = Arrays.toString(colour_code);
-        Property color = Structs.Property.newBuilder().setKey("rgb_color").setValue(new_colour1).build();
-        Segment connected1 = Segment.newBuilder().addProperties(segment_tail_coords).addProperties(segment_head_coords).addProperties(segment_middle_coords).addProperties(color).build();
+        Property color = Property.newBuilder().setKey("rgb_color").setValue(new_colour1).build();
+        Segment connected1 = Segment.newBuilder().addProperties(segment_tail_coords).addProperties(segment_head_coords).addProperties(segment_middle_coords).addProperties(color).addProperties(thicc).addProperties(a).addProperties(segment_id).build();
         return connected1;
     }
 
