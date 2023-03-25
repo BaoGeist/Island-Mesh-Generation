dfc214fdd2837cc5ed99342cfec7b8c20e957862
Daniel Hu <65648964+danielsphu@users.noreply.github.com>
2023-03-25 17:14:04 -0400

    F12 Added test cases for MathUtils

--- island/src/test/java/islandADTTest/tests/MathUtilsTest.java
+++ island/src/test/java/islandADTTest/tests/MathUtilsTest.java
@@ -6,11 +6,15 @@ import islandADTTest.tags.TestActual;
 import java.util.ArrayList;
 import java.util.List;
 
-import static islandADT.Utils.MathUtils.average;
+import static islandADT.Utils.MathUtils.*;
 import static islandADTTest.Assertions.assertTrue;
 
 public class MathUtilsTest {
     List<Integer> integers = new ArrayList<>();
+    double[] coords1 = new double[0];
+    double[] coords2 = new double[0];
+
+    double[] coords = new double[0];
 
     @Before
     public void SetUpIntegers() {
@@ -19,10 +23,37 @@ public class MathUtilsTest {
         integers.add(20);
         integers.add(10);
         integers.add(5);
+        System.out.println(average(integers) == 9);
+    }
+
+    @Before
+    public void SetUpTwoPoints(){
+        coords1[0] = 2;
+        coords1[1] = 4;
+        coords2[0] = 2;
+        coords2[1] = 2;
+        System.out.println(distance_between_points(coords1, coords2) == 2);
+    }
+
+    @Before
+    public void SetupCartesian(){
+        coords[0] = 1;
+        coords[1] = 90;
+        System.out.println(cartesian_from_polar(coords).equals(new double[]{0, 1}));
     }
 
     @TestActual
     public void test_average() {
         assertTrue(average(integers) == 9);
+    }
+
+    @TestActual
+    public void test_distance_calc(){
+        assertTrue(distance_between_points(coords1, coords2) == 2);
+    }
+
+    @TestActual
+    public void test_cartesian_from_polar(){
+        assertTrue(cartesian_from_polar(coords).equals(new double[]{0, 1}));
     }
 }
